7edfdf369e7f94987a56fec3d36cb2aed451d03a
Vince Au <vinceau09@gmail.com>
2019-11-23 13:16:19 +1100

    Remove stuff from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,7 @@
-import { SlippiRealtime } from './realtime/realtime';
-
 export * from './melee/characters';
 export * from './melee/moves';
 export * from './melee/stages';
-export * from "./utils/slpStream"
 
-const r = new SlippiRealtime({
-  address: "0.0.0.0",
-  port: 1667,
-  writeSlpFiles: false,
-  writeSlpFileLocation: ".",
-});
+export * from "./utils/slpStream";
 
-r.on("gameStart", () => {
-  console.log("game started");
-});
-r.on("gameEnd", () => {
-  console.log("game ended");
-});
-
-r.on('spawn', () => {
-  console.log('spawn');
-});
-r.on('death', () => {
-  console.log('death');
-});
-r.on("comboStart", () => {
-  console.log("comboStart");
-});
-r.on("comboExtend", () => {
-  console.log("comboExtend");
-});
-r.on("comboEnd", () => {
-  console.log("comboEnd");
-});
-
-r.start();
+export * from './realtime/realtime';
