23b7488c51c5009dd79931effe3bf8628dc3540b
Vince Au <vinceau09@gmail.com>
2019-11-27 09:54:53 +1100

    Add combo calculation

---
+++ src/combo/combo.ts
@@ -0,0 +1,47 @@
+import { Character } from "../melee/characters";
+import { ComboType, GameStartType } from "slp-parser-js";
+
+export interface ComboFilterOptions {
+  chainGrabbers: Character[];
+  nameTags: string[];
+  minComboPercent: number;
+  excludeCPUs: boolean;
+  excludeChainGrabs: boolean;
+  excludeWobbles: boolean;
+  largeHitThreshold: number; // The proportion of damage that a hit has to do to be considered a large hit
+  wobbleTreshold: number; // The number of pummels before it's considered a wobble
+  chainGrabThreshold: number; // proportion of up throw / pummels to other moves to be considered a chain grab
+  perCharacterMinComboPercent: { [characterId: number]: number };
+}
+
+export interface Criteria {
+  check: (combo: ComboType, settings: GameStartType, options: ComboFilterOptions) => boolean;
+}
+
+
+// const largeHitThreshold = 0.8;
+// const wobbleThreshold = 8;
+// const chainGrabThreshold = 0.8;
+// const chainGrabbers = [Character.MARTH, Character.PEACH, Character.PIKACHU, Character.DR_MARIO];
+
+export class ComboFilter {
+  private options: ComboFilterOptions;
+  private criteria: Criteria[];
+
+  public constructor(options: ComboFilterOptions) {
+    this.options = options;
+  }
+
+  public isCombo(combo: ComboType, settings: GameStartType): boolean {
+    // Check if we satisfy all the criteria
+    this.criteria.forEach((c) => {
+      if (!c.check(combo, settings, this.options)) {
+        return false;
+      }
+    });
+
+    // If the combo killed then it's a valid combo
+    return combo.didKill;
+  }
+}
+
---
+++ src/combo/criteria.ts
@@ -0,0 +1,105 @@
+import _ from "lodash";
+
+import { Criteria, ComboFilterOptions } from "./combo";
+import { ComboType, GameStartType } from "slp-parser-js";
+import { MoveID } from "../melee/moves";
+import { Character } from "../melee/characters";
+
+export class MatchesPlayerName implements Criteria {
+  public check(combo: ComboType, settings: GameStartType, options: ComboFilterOptions): boolean {
+    if (options.nameTags.length === 0) {
+      return true;
+    }
+         
+    const player = _.find(settings.players, (player) => player.playerIndex === combo.playerIndex);
+    options.nameTags.forEach((filterName) => {
+      const playerTag = player.nametag || null;
+      if (playerTag === filterName) {
+        // Continue processing if it matches the name tag
+        return true;
+      }
+    });
+    // Combo did not match a name
+    return false;
+  }
+}
+
+export class ExcludesChainGrabs implements Criteria {
+  public check(combo: ComboType, settings: GameStartType, options: ComboFilterOptions): boolean {
+    if (!options.excludeChainGrabs) {
+      return true;
+    }
+         
+    const player = _.find(settings.players, (player) => player.playerIndex === combo.playerIndex);
+    if (!options.chainGrabbers.includes(player.characterId)) {
+      return true;
+    }
+
+    const numUpThrowPummels = combo.moves.filter(({ moveId }) => moveId === MoveID.U_THROW || moveId === MoveID.GRAB_PUMMEL).length;
+    const numMoves = combo.moves.length;
+    const isChainGrab = numUpThrowPummels / numMoves >= options.chainGrabThreshold;
+    // Continue with processing if the combo is not a chain grab
+    return !isChainGrab;
+  }
+}
+
+export class ExcludesWobbles implements Criteria {
+  public check(combo: ComboType, settings: GameStartType, options: ComboFilterOptions): boolean {
+    if (!options.excludeWobbles) {
+      return true;
+    }
+         
+    const player = _.find(settings.players, (player) => player.playerIndex === combo.playerIndex);
+    if (player.characterId !== Character.ICE_CLIMBERS) {
+      // Continue processing if the character is not Ice Climbers
+      return true;
+    }
+
+    const wobbles = [];
+    let pummels = 0;
+    combo.moves.forEach(({ moveId }) => {
+      if (moveId === MoveID.GRAB_PUMMEL) {
+        pummels++;
+      } else {
+        wobbles.push(pummels);
+        pummels = 0;
+      }
+    });
+    wobbles.push(pummels);
+    const wobbled = _.some(wobbles, (pummelCount) => pummelCount > options.wobbleTreshold);
+    // Only continue processing of the combo is not a wobble
+    return !wobbled;
+  }
+}
+
+export class SatisfiesMinComboPercent implements Criteria {
+  public check(combo: ComboType, settings: GameStartType, options: ComboFilterOptions): boolean {
+    const player = _.find(settings.players, (player) => player.playerIndex === combo.playerIndex);
+
+    const minComboPercent = options.perCharacterMinComboPercent[player.characterId] || options.minComboPercent;
+    const totalComboPercent = combo.endPercent - combo.startPercent
+    // Continue only if the total combo percent was greater than the threshold
+    return totalComboPercent > minComboPercent;
+  }
+}
+
+export class ExcludesLargeSingleHit implements Criteria {
+  public check(combo: ComboType, settings: GameStartType, options: ComboFilterOptions): boolean {
+    const totalDmg = _.sumBy(combo.moves, ({ damage }) => damage);
+    const largeSingleHit = _.some(combo.moves, ({ damage }) => damage / totalDmg >= options.largeHitThreshold);
+    return !largeSingleHit;
+  }
+}
+
+export class ExcludesCPUs implements Criteria {
+  public check(combo: ComboType, settings: GameStartType): boolean {
+    const cpu = _.some(settings.players, (player) => player.type != 0)
+    return !cpu;
+  }
+}
+
+export class IsOneVsOne implements Criteria {
+  public check(combo: ComboType, settings: GameStartType): boolean {
+    return settings.players.length === 2;
+  }
+}
