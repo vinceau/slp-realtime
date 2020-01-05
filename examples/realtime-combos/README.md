6855fe2e4491ba764f7077df07395f0517d965f0
Vince Au <vinceau09@gmail.com>
2020-01-05 21:26:46 +1100

    Initial examples

---
+++ examples/realtime-combos/README.md
@@ -0,0 +1,9 @@
+# Real-time Combo Extraction Example
+
+An example demonstrating the real-time capabilities of `slp-realtime`.
+
+```
+yarn install
+yarn run build
+node src/index.js
+```
---
+++ examples/realtime-combos/src/index.js
@@ -0,0 +1,34 @@
+const { ConnectionStatus, SlippiLivestream, ComboFilter, DolphinComboQueue } = require("@vinceau/slp-realtime");
+
+// TODO: Make sure you set these values!
+const ADDRESS = "localhost";  // leave as is if the relay is on the same computer
+const PORT = 1667;            // relay port
+
+const outputCombosFile = "combos.js";
+
+let comboQueue;
+
+const livestream = new SlippiLivestream();
+livestream.start(ADDRESS, PORT)
+  .then(() => {
+    console.log("Connected to Slippi Relay");
+  })
+  .catch(console.error);
+
+
+livestream.connection.on("statusChange", (status) => {
+  if (status === ConnectionStatus.CONNECTED) {
+    console.log("Detected status change - Connected");
+    // Create a new combo queue
+    comboQueue = new DolphinComboQueue();
+  } else if (status === ConnectionStatus.DISCONNECTED) {
+    // We got disconnected from the relayer
+    console.log("Disconnected from the relay. Writing combo files...");
+    // Write out the combos
+    if (comboQueue) {
+      comboQueue.writeFile(outputCombosFile);
+      console.log(`Wrote ${comboQueue.length()} combos to ${outputCombosFile}`);
+    }
+  }
+});
+
