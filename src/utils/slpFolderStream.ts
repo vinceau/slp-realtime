6ec6fe44925bd127008f4489b2c1d91f377a5dcf
Vince Au <vinceau09@gmail.com>
2020-01-08 22:09:25 +1100

    Update folder stream

--- src/utils/slpFolderStream.ts
+++ src/utils/slpFolderStream.ts
@@ -6,34 +6,41 @@ import tailstream from "tailstream";
 import { SlpStream } from "./slpStream";
 
 export class SlpFolderStream extends SlpStream {
+  private readStream: any = null;
+
   public constructor(slpFolder: string) {
     super();
+    this._startWatching(slpFolder);
+  }
+
+  public end(): void {
+    super.end();
+    this.readStream.done();
+  }
+
+  private _startWatching(slpFolder: string): void {
     const initialFiles = fs.readdirSync(slpFolder).map(file =>
       path.resolve(path.join(slpFolder, file))
     );
-    console.log("found: ", initialFiles);
-
     const slpGlob = path.join(slpFolder, "*.slp");
-    let readStream: any = null;
 
     // Initialize watcher.
     const watcher = chokidar.watch(slpGlob, {
       ignored: /(^|[\/\\])\../, // ignore dotfiles
       persistent: true
     });
-    watcher
-      .on("add", path => {
-        if (initialFiles.includes(path)) {
-          console.log(`${path} is an old file`);
-        } else {
-          console.log(`File ${path} is a new file`)
-          if (readStream) {
-            // End the stream
-            readStream.done()
-          }
-          readStream = tailstream.createReadStream(path);
-          readStream.on("data", (data: any) => this.write(data));
-        }
-      });
+    watcher.on("add", path => {
+      if (initialFiles.includes(path)) {
+        return;
+      }
+      // This is a newly generated file
+      if (this.readStream) {
+        // End the old stream
+        this.readStream.done();
+      }
+      // Create a new stream for the new file
+      this.readStream = tailstream.createReadStream(path);
+      this.readStream.on("data", (data: any) => this.write(data));
+    });
   }
 }
