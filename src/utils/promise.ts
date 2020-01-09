73b4a39f10e17e4fe881429d791f35e18cdbf3c5
Vince Au <vinceau09@gmail.com>
2020-01-10 00:03:51 +1100

    Don't read folder synchronously

--- src/utils/promise.ts
+++ src/utils/promise.ts
@@ -1,3 +1,5 @@
+import fs from "fs";
+
 export const sleep = (ms: number): Promise<void> => new Promise((resolve): NodeJS.Timeout => setTimeout(resolve, ms))
 
 export const promiseTimeout = <T>(ms: number, promise: Promise<T>): Promise<T> => {
@@ -11,4 +13,16 @@ export const promiseTimeout = <T>(ms: number, promise: Promise<T>): Promise<T> =
 
   // Returns a race between our timeout and the passed in promise
   return Promise.race([promise, timeout]) as Promise<T>;
+}
+
+export const readDirAsync = (folderPath: string, options?: any): Promise<string[]> => {
+  return new Promise((resolve, reject): void => {
+    fs.readdir(folderPath, options, (err, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(files);
+      }
+    });
+  })
 }
