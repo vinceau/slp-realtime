6a19c8d517c3f0431a3251b5e62fc2d367ebc9ab
Vince Au <vinceau09@gmail.com>
2020-02-02 00:24:12 +1100

    Make inputs observable super general

--- src/events/inputs.ts
+++ src/events/inputs.ts
@@ -1,13 +1,20 @@
 import { SlpStream } from "../utils/slpStream";
-import { map, filter, distinctUntilChanged, mapTo } from "rxjs/operators";
+import { map, distinctUntilChanged } from "rxjs/operators";
 import { Observable, merge } from "rxjs";
 import { playerFilter } from "../operators/frames";
 
 // Export the parameter types for events
 export { GameStartType, GameEndType, ComboType, StockType, ConversionType } from "slp-parser-js";
 
+export interface ButtonComboPayload {
+  playerIndex: number;
+  buttonCombo: number;
+}
+
 export class InputEvents {
   protected stream: SlpStream | null = null;
+
+  public playerButtonCombo$: Observable<ButtonComboPayload>;
 
   /**
    * Starts listening to the provided stream for Slippi events
@@ -17,32 +24,27 @@ export class InputEvents {
    */
   public setStream(stream: SlpStream): void {
     this.stream = stream;
-  }
-
-  public playerButtonCombo(controlBitMask: number): Observable<number> {
-    if (!this.stream) {
-      throw new Error("No stream to subscribe to");
-    }
-
-    return merge(
-      this.playerIndexButtonCombo(0, controlBitMask),
-      this.playerIndexButtonCombo(1, controlBitMask),
-      this.playerIndexButtonCombo(2, controlBitMask),
-      this.playerIndexButtonCombo(3, controlBitMask),
+    this.playerButtonCombo$ = merge(
+      this.playerIndexButtonCombo(0),
+      this.playerIndexButtonCombo(1),
+      this.playerIndexButtonCombo(2),
+      this.playerIndexButtonCombo(3),
     );
   }
 
-  public playerIndexButtonCombo(index: number, controlBitMask: number): Observable<number> {
+  public playerIndexButtonCombo(index: number): Observable<ButtonComboPayload> {
     if (!this.stream) {
       throw new Error("No stream to subscribe to");
     }
 
     return this.stream.playerFrame$.pipe(
       playerFilter(index),
-      map(f => f.players[index].pre.physicalButtons & controlBitMask),
+      map(f => f.players[index].pre.physicalButtons),
       distinctUntilChanged(),
-      filter(n => n === controlBitMask),
-      mapTo(index),
+      map(buttonCombo  => ({
+        playerIndex: index,
+        buttonCombo,
+      })),
     );
   }
 
