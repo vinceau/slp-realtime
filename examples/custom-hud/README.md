ab798879590426b4d70e90d423ac881d7b8be01f
Vince Au <vinceau09@gmail.com>
2020-02-03 18:25:58 +1100

    Add custom hud example files

---
+++ examples/custom-hud/README.md
@@ -0,0 +1,37 @@
+# SLP Folder Monitoring Example
+
+This is an example demonstrating the real-time capabilities of `slp-realtime` and its ability
+to read events from open SLP files that are still be written to.
+
+This script reads events from a specific folder which has new SLP files being written to
+and generates a Dolphin-compatible `combos.json` file when disconnected the interrupt signal
+is given.
+
+## How to use
+
+1. Clone the repo, install the dependencies, and build the library to be used by the example.
+
+```bash
+git clone https://github.com/vinceau/slp-realtime
+cd slp-realtime
+yarn install
+yarn run build
+```
+
+2. Prepare the example by changing directory into the example folder and installing the dependencies. 
+
+```bash
+cd examples/folder-monitoring
+yarn install
+```
+
+3. Set the SLP folder folder location
+
+Open up the `index.js` file in your favourite editor, and modify the value for `slpLiveFolderPath` appropriately.
+This should be the folder that Slippi Dolphin writes new SLP files to.
+
+4. Run the actual code
+
+```bash
+node index.js
+```
---
+++ examples/custom-hud/index.js
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/*
+This example script reads live slp files from a folder, automatically
+detects combos, and generates a Dolphin-compatible `combos.json` file
+when the interrupt signal is detected.
+*/
+
+// TODO: Set this folder!!
+const playerInfoFolder = "C:\\Users\\Vince\\Desktop";
+
+const { SlpFolderStream, SlpRealTime } = require("@vinceau/slp-realtime");
+const fs = require("fs");
+const path = require("fs");
+
+// TODO: Make sure you set this value!
+const slpLiveFolderPath = "C:\\Users\\Vince\\Documents\\FM-v5.9-Slippi-r18-Win\\Slippi";
+console.log(`Monitoring ${slpLiveFolderPath} for new SLP files`);
+
+// Connect to the relay
+const stream = new SlpFolderStream();
+
+// Add the combos to the queue whenever we detect them
+const realtime = new SlpRealTime();
+realtime.setStream(stream);
+realtime.game.start$.subscribe(() => {
+  console.log(`Detected a new game in ${stream.getCurrentFilename()}`);
+});
+realtime.stock.percentChange$.subscribe((payload) => {
+  fs.writeFileSync(path.join(playerInfoFolder, `player${payload.playerIndex}Percent.txt`), payload.percent);
+});
+
+realtime.stock.countChange$.subscribe((payload) => {
+  fs.writeFileSync(path.join(playerInfoFolder, `player${payload.playerIndex}Stocks.txt`), payload.stocksRemaining);
+});
+
+// Start monitoring the folder for changes
+stream.start(slpLiveFolderPath);
