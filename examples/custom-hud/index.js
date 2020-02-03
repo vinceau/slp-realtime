a076cac7bd43d485473fb5536e7a2549b4bf46f8
Vince Au <vinceau09@gmail.com>
2020-02-03 19:20:44 +1100

    Write files asynchronously and add error handler

--- examples/custom-hud/index.js
+++ examples/custom-hud/index.js
@@ -18,6 +18,12 @@ console.log(`Monitoring ${slpLiveFolderPath} for new SLP files`);
 // Connect to the relay
 const stream = new SlpFolderStream();
 
+const errHandler = (err) => {
+  if (err) {
+    console.error(err);
+  }
+}
+
 // Add the combos to the queue whenever we detect them
 const realtime = new SlpRealTime();
 realtime.setStream(stream);
@@ -25,11 +31,21 @@ realtime.game.start$.subscribe(() => {
   console.log(`Detected a new game in ${stream.getCurrentFilename()}`);
 });
 realtime.stock.percentChange$.subscribe((payload) => {
-  fs.writeFileSync(path.join(playerInfoFolder, `player${payload.playerIndex}Percent.txt`), payload.percent);
+  console.log(`player ${payload.playerIndex + 1} percent: ${payload.percent}`);
+  fs.writeFile(path.join(playerInfoFolder, `player${payload.playerIndex + 1}Percent.txt`), `${Math.floor(payload.percent)}%`, errHandler);
 });
 
 realtime.stock.countChange$.subscribe((payload) => {
-  fs.writeFileSync(path.join(playerInfoFolder, `player${payload.playerIndex}Stocks.txt`), payload.stocksRemaining);
+  console.log(`player ${payload.playerIndex + 1} stocks: ${payload.stocksRemaining}`);
+  fs.writeFile(path.join(playerInfoFolder, `player${payload.playerIndex + 1}Stocks.txt`), payload.stocksRemaining, errHandler);
+});
+
+// Reset the text files on game end
+realtime.game.end$.subscribe(() => {
+  fs.writeFile(path.join(playerInfoFolder, `player1Stocks.txt`), "", errHandler);
+  fs.writeFile(path.join(playerInfoFolder, `player2Stocks.txt`), "", errHandler);
+  fs.writeFile(path.join(playerInfoFolder, `player1Percent.txt`), "", errHandler);
+  fs.writeFile(path.join(playerInfoFolder, `player2Percent.txt`), "", errHandler);
 });
 
 // Start monitoring the folder for changes
