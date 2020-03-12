b9bb38e2480cfaf7f7afc46c6194ebdd4eb904ac
Vince Au <vinceau09@gmail.com>
2020-03-12 13:42:51 +1100

    Add holding of inputs for a specified amount of frames

--- src/events/inputs.ts
+++ src/events/inputs.ts
@@ -1,8 +1,10 @@
 import { SlpStream } from "../utils/slpStream";
-import { map, distinctUntilChanged } from "rxjs/operators";
+import { map, scan, filter, mapTo } from "rxjs/operators";
 import { Observable, Subject, Subscription } from "rxjs";
 import { playerFilter } from "../operators/frames";
 import { forAllPlayerIndices } from "../utils/helpers";
+import { ControllerInput } from "../types";
+import { generateControllerBitmask } from "../utils";
 
 // Export the parameter types for events
 export { GameStartType, GameEndType, ComboType, StockType, ConversionType } from "slp-parser-js";
@@ -36,19 +38,40 @@ export class InputEvents {
     );
   }
 
-  public playerIndexButtonCombo(index: number): Observable<ButtonComboPayload> {
+  /**
+   * Returns an observable which emits when `buttons` is held for `duration` number of frames.
+   *
+   * @param {number} index The player index
+   * @param {number} buttons The button combination
+   * @param {number} duration The number of frames the buttons were held for
+   * @returns {Observable<number>}
+   * @memberof InputEvents
+   */
+  public playerIndexButtonCombo(index: number, buttons: ControllerInput[], duration = 1): Observable<number> {
     if (!this.stream) {
       throw new Error("No stream to subscribe to");
     }
 
+    const controlBitMask = generateControllerBitmask(...buttons);
     return this.stream.playerFrame$.pipe(
       playerFilter(index),
-      map(f => f.players[index].pre.physicalButtons),
-      distinctUntilChanged(),
-      map(buttonCombo  => ({
-        playerIndex: index,
-        buttonCombo,
-      })),
+      // Map the frames to whether the button combination was pressed or not
+      map(f => {
+        const buttonCombo = f.players[index].pre.physicalButtons;
+        const buttonComboPressed = (buttonCombo & controlBitMask) === controlBitMask;
+        return buttonComboPressed;
+      }),
+      // Count the number of consecutively pressed frames
+      scan((acc, pressed) => {
+        if (pressed) {
+          return acc + 1;
+        }
+        return 0;
+      }, 0),
+      // Filter to be the exact frame when we pressed the combination for sufficient frames
+      filter(n => n === duration),
+      // Return the player index which triggered the button press
+      mapTo(index),
     );
   }
 
