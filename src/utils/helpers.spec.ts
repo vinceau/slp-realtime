3b988648eaa40da42cb63fe87819988c3e97c7ec
Vince Au <vinceau09@gmail.com>
2020-04-04 10:41:38 +1100

    Simplify test frame data generation

--- src/utils/helpers.spec.ts
+++ src/utils/helpers.spec.ts
@@ -5,86 +5,73 @@ import { findWinner } from "./helpers";
 describe("when calculating the game winner", () => {
 
     it("should choose the player with the most stocks", async () => {
-        const lastFrame: FrameEntryType = {
-            frame: 123,
-            players: {
-                0: {
-                    post: {
-                        frame: 123,
-                        playerIndex: 0,
-                        percent: 100,
-                        stocksRemaining: 0,
-                    },
-
-                },
-                1: {
-                    post: {
-                        frame: 123,
-                        playerIndex: 1,
-                        percent: 100,
-                        stocksRemaining: 1,
-                    },
-
-                },
+        const lastFrame = generateLastFrame([
+            {
+                playerIndex: 0,
+                percent: 100,
+                stocksRemaining: 0,
             },
-        } as unknown as FrameEntryType;
+            {
+                playerIndex: 1,
+                percent: 100,
+                stocksRemaining: 1,
+            },
+        ]);
         const winnerIndex = findWinner(lastFrame);
         expect(winnerIndex).toEqual(1);
     });
 
     it("should choose the player with the least percent if stocks are tied", async () => {
-        const lastFrame: FrameEntryType = {
-            frame: 123,
-            players: {
-                1: {
-                    post: {
-                        frame: 123,
-                        playerIndex: 1,
-                        percent: 100,
-                        stocksRemaining: 1,
-                    },
-
-                },
-                3: {
-                    post: {
-                        frame: 123,
-                        playerIndex: 3,
-                        percent: 90,
-                        stocksRemaining: 1,
-                    },
-
-                },
+        const lastFrame = generateLastFrame([
+            {
+                playerIndex: 1,
+                percent: 100,
+                stocksRemaining: 1,
             },
-        } as unknown as FrameEntryType;
+            {
+                playerIndex: 3,
+                percent: 90,
+                stocksRemaining: 1,
+            }
+        ]);
         const winnerIndex = findWinner(lastFrame);
         expect(winnerIndex).toEqual(3);
     });
 
     it("should choose the player with the lowest port number if stocks and percent are tied", async () => {
-        const lastFrame: FrameEntryType = {
-            frame: 123,
-            players: {
-                1: {
-                    post: {
-                        frame: 123,
-                        playerIndex: 1,
-                        percent: 100,
-                        stocksRemaining: 1,
-                    },
-
-                },
-                3: {
-                    post: {
-                        frame: 123,
-                        playerIndex: 3,
-                        percent: 100,
-                        stocksRemaining: 1,
-                    },
-
-                },
+        const lastFrame = generateLastFrame([
+            {
+                playerIndex: 1,
+                percent: 100,
+                stocksRemaining: 1,
             },
-        } as unknown as FrameEntryType;
+            {
+                playerIndex: 3,
+                percent: 100,
+                stocksRemaining: 1,
+            }
+        ]);
         const winnerIndex = findWinner(lastFrame);
         expect(winnerIndex).toEqual(1);
     });
 });
+
+interface PlayerInfo {
+    playerIndex: number;
+    percent: number;
+    stocksRemaining: number;
+}
+
+const generateLastFrame = (playerInfo: PlayerInfo[]): FrameEntryType => {
+    const players: any = {};
+    for (let p of playerInfo) {
+        players[p.playerIndex] = {
+            post: p
+        };
+    }
+    const lastFrame: FrameEntryType = {
+        frame: 123,
+        players,
+    } as unknown as FrameEntryType;
+    return lastFrame;
+}
