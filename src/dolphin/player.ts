79562549964f1c0401f81b5412733eb209121c4c
Vince Au <vinceau09@gmail.com>
2020-04-04 00:11:56 +1100

    Clean up dolphin code

--- src/dolphin/player.ts
+++ src/dolphin/player.ts
@@ -10,27 +10,13 @@
  * [NO_GAME] no more files in the queue
  */
 
-import { Observable, ReplaySubject, from, Subscription, fromEventPattern, Subject, merge } from "rxjs";
-import fs from "fs-extra";
+import { ReplaySubject, Subject, merge } from "rxjs";
 
 import { ChildProcess, execFile } from "child_process";
-import { observableDolphinProcess, DolphinPlaybackInfo } from "./playback";
-import { DolphinEntry, DolphinQueueFormat } from "../utils/dolphin";
-import { tap, switchMap, map, takeUntil } from "rxjs/operators";
-import { Frames } from "slp-parser-js";
+import { observableDolphinProcess } from "./playback";
+import { takeUntil } from "rxjs/operators";
 
 const MAX_BUFFER = 2 ** 20;
-const DELAY_AMOUNT_MS = 1000;
-
-const START_RECORDING_BUFFER = 90;
-const END_RECORDING_BUFFER = -60;
-
-interface IDolphinPlayer {
-    loadJSON: () => void;
-    // loadSlpFiles: (files: string[]) => void;
-    commands$: Observable<any>;
-    currentFile: () => string;
-}
 
 interface DolphinLauncherOptions {
     dolphinPath: string;
@@ -73,16 +59,6 @@ export class DolphinLauncher {
 
     public constructor(options: Partial<DolphinLauncherOptions>) {
         this.options = Object.assign({}, defaultDolphinLauncherOptions, options);
-        /*
-        this.currentJSONFile$ = this.currentJSONFileSource.pipe(
-            switchMap(filename => from<Promise<DolphinQueueFormat>>(fs.readJSON(filename)).pipe(
-                map(contents => ({
-                    filename,
-                    contents,
-                })),
-            )),
-        );
-        */
     }
 
     public loadJSON(comboFilePath: string): ChildProcess {
@@ -92,7 +68,6 @@ export class DolphinLauncher {
         }
         this._resetState();
 
-        // this.currentJSONFileSource.next(comboFilePath);
         this.dolphin = this._executeFile(comboFilePath);
 
         this.dolphin.on("close", () => {
@@ -146,32 +121,11 @@ export class DolphinLauncher {
         return execFile(this.options.dolphinPath, params, { maxBuffer: MAX_BUFFER });
     }
 
-    /*
-    private _handleRecording(dolphin: ChildProcess, shouldPause?: boolean) {
-        const obsConnected = obsConnection.isConnected();
-        if (!obsConnected) {
-            console.error("OBS is not connected. Not recording Dolphin.");
-            return;
-        }
-        if (dolphin.stdout) {
-            dolphin.stdout.on("data", (data: string) => {
-                this._handleStdoutData(data);
-            });
-        }
-        dolphin.on("close", async () => {
-            // TODO: unsubscribe from subscription
-            // TODO: emit dolphin close event
-        });
-    }
-    */
-
     private _handleCurrentFrame(commandValue: number) {
         this.currentFrame = commandValue;
         if (this.currentFrame === this.startRecordingFrame) {
             this.gameStartSource.next();
-            // console.log("game start");
         } else if (this.currentFrame === this.endRecordingFrame) {
-            // console.log("game end");
             this.gameEndSource.next({
                 gameEnded: this.waitForGAME,
             });
@@ -182,7 +136,6 @@ export class DolphinLauncher {
     private _handlePlaybackStartFrame(commandValue: number) {
         // Ensure the start frame is at least bigger than the intital playback start frame
         this.startRecordingFrame = Math.max(commandValue, commandValue + this.options.startBuffer);
-        // console.log(`StartFrame: ${this.startRecordingFrame}`);
     }
 
     private _handlePlaybackEndFrame(commandValue: number) {
@@ -192,18 +145,14 @@ export class DolphinLauncher {
         // Ensure the adjusted frame is between the start and end frames
         const adjustedEndFrame = Math.max(this.startRecordingFrame, this.endRecordingFrame - this.options.endBuffer);
         this.endRecordingFrame = Math.min(adjustedEndFrame, this.lastGameFrame);
-        // console.log(`EndFrame: ${this.endRecordingFrame}`);
     }
 
     private _handleGameEndFrame(commandValue: number) {
         this.lastGameFrame = commandValue;
-        // console.log(`LastFrame: ${this.lastGameFrame}`);
     }
 
     private _handleNoGame() {
         this.queueEmptySource.next();
-        // console.log("No games remaining in queue");
-        // console.log("Stopping Recording");
     }
 
     private _resetState() {
@@ -214,42 +163,3 @@ export class DolphinLauncher {
         this.waitForGAME = false;
     }
 }
-
-/*
-const dolphinPlayer = new DolphinPlayer();
-
-export const openComboInDolphin = (filePath: string, record?: boolean) => {
-    dolphinPlayer.loadJSON(filePath, { record });
-};
-
-interface DolphinPlaybackPayload {
-    command: string;
-    value: string;
-}
-
-const observableDolphinProcess = (dolphinStdout: Readable): Observable<DolphinPlaybackPayload> => {
-    const lines$ = fromEventPattern(
-        handler => dolphinStdout.addListener("data", handler),
-        handler => dolphinStdout.removeListener("data", handler),
-        (data: string) => data.split(os.EOL).filter(line => Boolean(line))
-    );
-    return lines$.pipe(
-        // Split each line into its own event
-        concatMap(lines => of(...lines)),
-        // Split the line into chunks
-        map(line => line.split(" ")),
-        // We must have at least 2 chunks
-        filter(chunks => chunks.length >= 2),
-        // Map to the playback payload
-        map(chunks => {
-            const [command, value] = chunks;
-            const payload: DolphinPlaybackPayload = {
-                command,
-                value,
-            };
-            return payload;
-        }),
-    );
-};
-
-*/
