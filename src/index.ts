d3caa5a079b1d626e2503cd521469e9fb25dc0a3
Vince Au <vinceau09@gmail.com>
2020-11-26 16:32:39 +1100

    Stop using default exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-import { SlpRealTime } from "./realtime";
-
 export * from "./manager";
 export * from "./operators";
 export * from "./realtime";
 export * from "./stream";
 export * from "./utils";
-
 export * from "./types";
-
-export default SlpRealTime;
