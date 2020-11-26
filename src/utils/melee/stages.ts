b7a2f1bee7e1cf0ca2a020ca6f47b0ed883ad0c1
Vince Au <vinceau09@gmail.com>
2020-11-26 16:47:55 +1100

    Only store stage short names

--- src/utils/melee/stages.ts
+++ src/utils/melee/stages.ts
@@ -1,4 +1,4 @@
-import { Stage } from "@slippi/slippi-js";
+import { Stage, stages as stageUtils } from "@slippi/slippi-js";
 
 export { Stage } from "@slippi/slippi-js";
 
@@ -8,140 +8,24 @@ export interface StageInfo {
   shortName?: string;
 }
 
-const stagesMap = new Map<Stage, StageInfo>()
-  .set(Stage.FOUNTAIN_OF_DREAMS, {
-    id: Stage.FOUNTAIN_OF_DREAMS,
-    name: "Fountain of Dreams",
-    shortName: "FoD",
-  })
-  .set(Stage.POKEMON_STADIUM, {
-    id: Stage.POKEMON_STADIUM,
-    name: "Pokémon Stadium",
-    shortName: "PS",
-  })
-  .set(Stage.PEACHS_CASTLE, {
-    id: Stage.PEACHS_CASTLE,
-    name: "Princess Peach's Castle",
-  })
-  .set(Stage.KONGO_JUNGLE, {
-    id: Stage.KONGO_JUNGLE,
-    name: "Kongo Jungle",
-  })
-  .set(Stage.BRINSTAR, {
-    id: Stage.BRINSTAR,
-    name: "Brinstar",
-  })
-  .set(Stage.CORNERIA, {
-    id: Stage.CORNERIA,
-    name: "Corneria",
-  })
-  .set(Stage.YOSHIS_STORY, {
-    id: Stage.YOSHIS_STORY,
-    name: "Yoshi's Story",
-    shortName: "YS",
-  })
-  .set(Stage.ONETT, {
-    id: Stage.ONETT,
-    name: "Onett",
-  })
-  .set(Stage.MUTE_CITY, {
-    id: Stage.MUTE_CITY,
-    name: "Mute City",
-  })
-  .set(Stage.RAINBOW_CRUISE, {
-    id: Stage.RAINBOW_CRUISE,
-    name: "Rainbow Cruise",
-  })
-  .set(Stage.JUNGLE_JAPES, {
-    id: Stage.JUNGLE_JAPES,
-    name: "Jungle Japes",
-  })
-  .set(Stage.GREAT_BAY, {
-    id: Stage.GREAT_BAY,
-    name: "Great Bay",
-  })
-  .set(Stage.HYRULE_TEMPLE, {
-    id: Stage.HYRULE_TEMPLE,
-    name: "Hyrule Temple",
-  })
-  .set(Stage.BRINSTAR_DEPTHS, {
-    id: Stage.BRINSTAR_DEPTHS,
-    name: "Brinstar Depths",
-  })
-  .set(Stage.YOSHIS_ISLAND, {
-    id: Stage.YOSHIS_ISLAND,
-    name: "Yoshi's Island",
-  })
-  .set(Stage.GREEN_GREENS, {
-    id: Stage.GREEN_GREENS,
-    name: "Green Greens",
-  })
-  .set(Stage.FOURSIDE, {
-    id: Stage.FOURSIDE,
-    name: "Fourside",
-  })
-  .set(Stage.MUSHROOM_KINGDOM, {
-    id: Stage.MUSHROOM_KINGDOM,
-    name: "Mushroom Kingdom I",
-  })
-  .set(Stage.MUSHROOM_KINGDOM_2, {
-    id: Stage.MUSHROOM_KINGDOM_2,
-    name: "Mushroom Kingdom II",
-  })
-  .set(Stage.VENOM, {
-    id: Stage.VENOM,
-    name: "Venom",
-  })
-  .set(Stage.POKE_FLOATS, {
-    id: Stage.POKE_FLOATS,
-    name: "Poké Floats",
-  })
-  .set(Stage.BIG_BLUE, {
-    id: Stage.BIG_BLUE,
-    name: "Big Blue",
-  })
-  .set(Stage.ICICLE_MOUNTAIN, {
-    id: Stage.ICICLE_MOUNTAIN,
-    name: "Icicle Mountain",
-  })
-  .set(Stage.ICETOP, {
-    id: Stage.ICETOP,
-    name: "Icetop",
-  })
-  .set(Stage.FLAT_ZONE, {
-    id: Stage.FLAT_ZONE,
-    name: "Flat Zone",
-  })
-  .set(Stage.DREAMLAND, {
-    id: Stage.DREAMLAND,
-    name: "Dream Land N64",
-    shortName: "DL",
-  })
-  .set(Stage.YOSHIS_ISLAND_N64, {
-    id: Stage.YOSHIS_ISLAND_N64,
-    name: "Yoshi's Island N64",
-  })
-  .set(Stage.KONGO_JUNGLE_N64, {
-    id: Stage.KONGO_JUNGLE_N64,
-    name: "Kongo Jungle N64",
-  })
-  .set(Stage.BATTLEFIELD, {
-    id: Stage.BATTLEFIELD,
-    name: "Battlefield",
-    shortName: "BF",
-  })
-  .set(Stage.FINAL_DESTINATION, {
-    id: Stage.FINAL_DESTINATION,
-    name: "Final Destination",
-    shortName: "FD",
-  });
+const shortNames = new Map<Stage, string>()
+  .set(Stage.FOUNTAIN_OF_DREAMS, "FoD")
+  .set(Stage.POKEMON_STADIUM, "PS")
+  .set(Stage.YOSHIS_STORY, "YS")
+  .set(Stage.DREAMLAND, "DL")
+  .set(Stage.BATTLEFIELD, "BF")
+  .set(Stage.FINAL_DESTINATION, "FD");
 
 export function getStageInfo(stageId: number): StageInfo {
-  const s = stagesMap.get(stageId);
+  const s = stageUtils.getStageInfo(stageId);
   if (!s) {
     throw new Error(`Invalid stage with id ${stageId}`);
   }
-  return s;
+  const shortName = shortNames.get(stageId);
+  return {
+    ...s,
+    shortName,
+  };
 }
 
 export function getStageName(stageId: number): string {
