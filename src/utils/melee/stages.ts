9fc470b88560195b2e2163076c8e0313dde633fd
Vince Au <vinceau09@gmail.com>
2020-11-26 16:35:15 +1100

    Use Stage and Character exports from slippi-js

--- src/utils/melee/stages.ts
+++ src/utils/melee/stages.ts
@@ -1,35 +1,6 @@
-export enum Stage {
-  FOUNTAIN_OF_DREAMS = 2,
-  POKEMON_STADIUM = 3,
-  PEACHS_CASTLE = 4,
-  KONGO_JUNGLE = 5,
-  BRINSTAR = 6,
-  CORNERIA = 7,
-  YOSHIS_STORY = 8,
-  ONETT = 9,
-  MUTE_CITY = 10,
-  RAINBOW_CRUISE = 11,
-  JUNGLE_JAPES = 12,
-  GREAT_BAY = 13,
-  HYRULE_TEMPLE = 14,
-  BRINSTAR_DEPTHS = 15,
-  YOSHIS_ISLAND = 16,
-  GREEN_GREENS = 17,
-  FOURSIDE = 18,
-  MUSHROOM_KINGDOM = 19,
-  MUSHROOM_KINGDOM_2 = 20,
-  VENOM = 22,
-  POKE_FLOATS = 23,
-  BIG_BLUE = 24,
-  ICICLE_MOUNTAIN = 25,
-  ICETOP = 26,
-  FLAT_ZONE = 27,
-  DREAMLAND = 28,
-  YOSHIS_ISLAND_N64 = 29,
-  KONGO_JUNGLE_N64 = 30,
-  BATTLEFIELD = 31,
-  FINAL_DESTINATION = 32,
-}
+import { Stage } from "@slippi/slippi-js";
+
+export { Stage } from "@slippi/slippi-js";
 
 export interface StageInfo {
   id: Stage;
