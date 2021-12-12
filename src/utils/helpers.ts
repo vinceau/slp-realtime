85e3946d8188c0de38e6fb0db2df9f426b7d7038
Milan van As <milanvanas@gmail.com>
2021-12-12 10:08:18 +0100

    Added pipeFileContentsToFile helper fn

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,7 +1,9 @@
 import fs from "fs";
+const brake = require("brake");
 import type { Observable } from "rxjs";
 import { merge } from "rxjs";
-import type { Writable } from "stream";
+import type { Readable, Writable } from "stream";
+const Transform = require("readable-stream").Transform;
 
 export const forAllPlayerIndices = <T>(func: (index: number) => Observable<T>): Observable<T> => {
   return merge(func(0), func(1), func(2), func(3));
@@ -15,6 +17,28 @@ export const pipeFileContents = async (filename: string, destination: Writable, 
     });
     readStream.on("close", () => {
       resolve();
+    });
+  });
+};
+
+export const pipeFileContentsToFile = async (
+  filename: string,
+  destination: string,
+  speed: Number = 3000,
+): Promise<{ readStream: Readable; writeStream: Writable; brakeStream: typeof Transform }> => {
+  return new Promise((resolve) => {
+    const readStream = fs.createReadStream(filename);
+
+    const writeStream = fs.createWriteStream(destination);
+
+    const brakeStream = brake(speed);
+
+    readStream.pipe(brakeStream).pipe(writeStream);
+
+    readStream.on("open", () => {
+      readStream.pipe(brakeStream).pipe(writeStream);
+
+      resolve({ readStream, writeStream, brakeStream });
     });
   });
 };
