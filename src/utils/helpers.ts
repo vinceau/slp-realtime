b8197a25ad6ef3b2abc77942bf5b571a3e64e00c
Milan van As <milanvanas@gmail.com>
2021-12-13 11:45:16 +0100

    Removed pipFileContentsToFile helper fn, no more need for it

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,8 +1,7 @@
 import fs from "fs";
-const brake = require("brake");
 import type { Observable } from "rxjs";
 import { merge } from "rxjs";
-import type { Readable, Writable } from "stream";
+import type { Writable } from "stream";
 
 export const forAllPlayerIndices = <T>(func: (index: number) => Observable<T>): Observable<T> => {
   return merge(func(0), func(1), func(2), func(3));
@@ -17,39 +16,5 @@ export const pipeFileContents = async (filename: string, destination: Writable, 
     readStream.on("close", () => {
       resolve();
     });
-  });
-};
-
-// TODO - Resolve when the stream has actually started
-export const pipeFileContentsToFile = async (
-  filename: string,
-  destination: string,
-  slow: boolean = true,
-): Promise<{
-  readStream: Readable;
-  writeStream: Writable;
-  brakeStream: typeof brake;
-  stopPipeStream: Function;
-}> => {
-  return new Promise((resolve) => {
-    const writeStream = fs.createWriteStream(destination);
-    const readStream = fs.createReadStream(filename);
-    // Brake will slow down the stream by the given speed, this is useful for testing SlpFolderStreams
-    const brakeStream = brake(2e4);
-
-    if (slow) {
-      readStream.pipe(brakeStream).pipe(writeStream);
-    } else {
-      readStream.pipe(writeStream);
-    }
-
-    let stopPipeStream = () => {
-      readStream.unpipe(writeStream).unpipe(brakeStream);
-      readStream.destroy();
-      writeStream.destroy();
-      brakeStream.destroy();
-    };
-
-    resolve({ readStream, writeStream, brakeStream, stopPipeStream });
   });
 };
