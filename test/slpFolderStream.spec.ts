25aaa8e59699059399efd65aa702cc647c381110
Milan van As <milanvanas@gmail.com>
2021-12-13 11:50:09 +0100

    BROKEN TEST: New setup for slpFolderStream test but there seems to be no way to wait for SlpFolderStream to be completely ready, pleaplease help lol

--- test/slpFolderStream.spec.ts
+++ test/slpFolderStream.spec.ts
@@ -1,12 +1,12 @@
 import path from "path";
 import fs from "fs";
 
-import { pipeFileContentsToFile } from "../src";
+import { SlpFolderStream } from "../src";
 
-const { SlpFolderStream } = require("../src");
+const targetDirectory = path.resolve(__dirname, "..", "slp", "folder-stream");
 
 describe("slpFolderStream", () => {
-  let tmpSlpFiles: Array<String>;
+  let tmpSlpFiles: Array<fs.PathLike>;
 
   beforeAll(() => {
     tmpSlpFiles = [];
@@ -16,11 +16,6 @@ describe("slpFolderStream", () => {
     tmpSlpFiles.forEach((file) => fs.unlinkSync(file));
   });
 
-  it("can be initialised", async () => {
-    const slpFolderStream = new SlpFolderStream();
-    expect(slpFolderStream).toBeInstanceOf(SlpFolderStream);
-  });
-
   it("starts watching, detects a new file and stops watching", async () => {
     const slpFolderStream = new SlpFolderStream();
 
@@ -28,28 +23,22 @@ describe("slpFolderStream", () => {
 
     // Generate a random targetFile
     let targetFile = path.resolve(targetDirectory, (Math.random() + 1).toString(36).substring(7) + ".slp");
-
-    // Push for cleanup
     tmpSlpFiles.push(targetFile);
 
-    // TODO - This is a hack to get around the folder stream not being ready yet before pipeFileContentsToFile starts writing to the targetFile
-    await new Promise((resolve) => setTimeout(resolve, 250));
-
-    // Pipe an slp file to the targetFile
-    const { stopPipeStream } = await pipeFileContentsToFile("slp/Game_20190810T162904.slp", targetFile);
-
-    // TODO - This is a hack to get around the folder stream not being ready yet before pipeFileContentsToFile starts writing to the targetFile
-    await new Promise((resolve) => setTimeout(resolve, 250));
+    // Copy a slp file to the target directory so slpFolderStream can pick it up
+    fs.copyFileSync("slp/Game_20190810T162904.slp", targetFile);
 
     expect(slpFolderStream.latestFile()).toBe(targetFile);
 
     // Stop the folder stream and watcher
     slpFolderStream.stop();
 
-    // After stopping, the watcher should be null
-    expect(slpFolderStream.getWatcher()).toBe(null);
+    // Generate a new random targetFile
+    targetFile = path.resolve(targetDirectory, (Math.random() + 1).toString(36).substring(7) + ".slp");
+    tmpSlpFiles.push(targetFile);
 
-    // Stop the tmp stream
-    stopPipeStream();
+    fs.copyFileSync("slp/Game_20190810T162904.slp", targetFile);
+
+    expect(slpFolderStream.latestFile()).not.toBe(targetFile);
   });
 });
